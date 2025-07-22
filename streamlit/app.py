305bcd6eb228b05cf89dc2f29167e4cf5d8ddd24
sntk-76 <sn.tvkl@gmail.com>
2025-07-22 18:17:26 +0000

    adjust the requirements files

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,13 +6,13 @@ import os
 from tensorflow.keras.models import load_model
 from data_collector import data_downloader
 from preprocessing import main as preprocess_pipeline
-from openai import OpenAI
+import openai
 from dotenv import load_dotenv
 from sheet_logger import log_to_google_sheet
 from google.oauth2.service_account import Credentials
 
 # --- 🔐 Load API Key ---
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # --- 🔮 Build Prompt for LLM ---
 def build_prompt(forecast_df,city):
