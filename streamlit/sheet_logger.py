7904c45bfb3e7bbeec866394642f66985e8f6644
sntk-76 <sn.tvkl@gmail.com>
2025-07-22 18:47:21 +0000

    Modify the sheet_logger file

--- streamlit/sheet_logger.py
+++ streamlit/sheet_logger.py
@@ -3,11 +3,22 @@ from google.oauth2.service_account import Credentials
 from datetime import datetime
 import streamlit as st
 
-def log_to_google_sheet(city, sheet_id="1OB_3v48cYCH7VaXd-JMGVg93q3ga1f3kMyKLIXvQmsA", creds_path="/home/sina.tvk.1997/AI-weather-predictor/authentication/service_account.json"):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = Credentials.from_service_account_info(st.secrets["gspread"])
-    client = gspread.authorize(creds)
+def log_to_google_sheet(
+    city,
+    sheet_id="1OB_3v48cYCH7VaXd-JMGVg93q3ga1f3kMyKLIXvQmsA"
+):
+    
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
 
+    creds = Credentials.from_service_account_info(
+        st.secrets["gspread"],
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
     sheet = client.open_by_key(sheet_id)
     worksheet = sheet.worksheet("Logs")
 
